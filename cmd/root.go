6b6c2e4653f9ee20d4677524809b9db02a618bd1
Sundaram Kumar Jha <jhasundaram@outlook.com>
2024-12-01 16:22:06 +0530

    feat: root cmd

--- cmd/root.go
+++ cmd/root.go
@@ -1,42 +1,98 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"github.com/sundaram2021/dockit/pkg/builder"
+	"github.com/sundaram2021/dockit/pkg/git"
 )
 
 var (
-	dockerfileURL string
-	sourceCodeURL string
-	rootCmd       = &cobra.Command{
-		Use:   "dockit",
-		Short: "dockit is a Simple Docker Tool",
-		Long: `dockit is a CLI tool that wraps Docker commands and helps to run Dockerfiles present without downloading source code.
-	dockit -d <dockerfile-link> -s <source-code-folder-link>`,
-	}
+	repoURL     string
+	branch      string
+	imageTag    string
+	outputDir   string
+	verbose     bool
 )
 
-func init() {
-	rootCmd.PersistentFlags().StringVarP(&dockerfileURL, "dockerfile", "d", "", "URL of the Dockerfile")
-	rootCmd.PersistentFlags().StringVarP(&sourceCodeURL, "source", "s", "", "URL of the source code folder")
-	rootCmd.MarkPersistentFlagRequired("dockerfile")
-	rootCmd.MarkPersistentFlagRequired("source")
+var rootCmd = &cobra.Command{
+	Use:   "dockit",
+	Short: "Dockit - A CLI tool to build Docker images from GitHub repositories",
+	Long: `Dockit simplifies the process of building Docker images directly from GitHub repositories.
+	
+Usage:
+  dockit [github-repo-url] [flags]
 
+Example:
+  dockit https://github.com/example/repo --branch main --tag my-image`,
+	Args: cobra.MaximumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		setupLogging()
+
+		if len(args) > 0 {
+			repoURL = args[0]
+		}
+
+		if repoURL == "" {
+			logrus.Fatal("GitHub repository URL is required")
+		}
+
+		if outputDir == "" {
+			tempDir, err := os.MkdirTemp("", "dockit-*")
+			if err != nil {
+				logrus.Fatalf("Failed to create temporary directory: %v", err)
+			}
+			outputDir = tempDir
+			defer os.RemoveAll(tempDir)
+		}
+
+		repoPath, err := git.CloneRepository(repoURL, branch, outputDir)
+		if err != nil {
+			logrus.Fatalf("Repository cloning failed: %v", err)
+		}
+
+		imageName := imageTag
+		if imageName == "" {
+			imageName = filepath.Base(repoURL)
+		}
+
+		err = builder.BuildDockerImage(repoPath, imageName)
+		if err != nil {
+			logrus.Fatalf("Docker image build failed: %v", err)
+		}
+
+		logrus.Infof("Successfully built Docker image: %s", imageName)
+	},
+}
+
+func setupLogging() {
 	logrus.SetFormatter(&logrus.TextFormatter{
 		FullTimestamp: true,
+		ForceColors:   true,
 	})
-	logrus.SetLevel(logrus.InfoLevel)
 
-	rootCmd.Run = func(cmd *cobra.Command, args []string) {
-		ExecuteDockerfile()
+	if verbose {
+		logrus.SetLevel(logrus.DebugLevel)
+	} else {
+		logrus.SetLevel(logrus.InfoLevel)
 	}
 }
 
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
-		logrus.Error(err)
+		fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+func init() {
+	rootCmd.Flags().StringVar(&repoURL, "url", "", "GitHub repository URL")
+	rootCmd.Flags().StringVar(&branch, "branch", "main", "Branch to clone")
+	rootCmd.Flags().StringVar(&imageTag, "tag", "", "Custom Docker image tag")
+	rootCmd.Flags().StringVar(&outputDir, "output", "", "Output directory for cloned repository")
+	rootCmd.Flags().BoolVar(&verbose, "verbose", false, "Enable verbose logging")
 }
